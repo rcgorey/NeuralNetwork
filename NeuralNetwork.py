250a3f77fd179b8cc809303f8d00bd27255e8518
goreyr <goreyr@carleton.edu>
2019-02-08 13:05:23 -0800

    Created NeuralNetwork.py file with a very rough skeleton. Needs proper structure and documentation.

---
+++ NeuralNetwork.py
@@ -0,0 +1,29 @@
+'''
+Neural Network
+
+Author: Ryan Gorey
+Created: 2/8/2019
+
+Defines structure and behaviors for a Neural Network object.
+
+Intended to be applied to the MNIST handwritten digit database.
+
+Created independently, using http://neuralnetworksanddeeplearning.com/ as a resource.
+'''
+
+class NeuralNetwork():
+
+    def __init__(self):
+        //weights = numpy matrix
+        //biases = numpy vector (matrix?)
+        //initialize hyperparameters to a default setting
+
+    def feed_forward(self):
+        return null
+
+    def train(self, training_data):
+        return self
+
+    def backpropagation(self):
+        return self
+
