b4cd15ee4fb1fcc756856930d74f64a8aa4bbfc4
goreyr <goreyr@carleton.edu>
2019-02-15 12:03:23 -0800

    Added logic to create a list of weight matrices depending on the desired dimensions of the neural network. Added an option to print the weight matrices.

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,4 +1,4 @@
-'''
+"""
 Neural Network
 
 Author: Ryan Gorey
@@ -8,15 +8,48 @@ Defines structure and behaviors for a Neural Network object.
 
 Intended to be applied to the MNIST handwritten digit database.
 
-Created independently, using http://neuralnetworksanddeeplearning.com/ as a resource.
-'''
+Created independently, using http://neuralnetworksanddeeplearning.com/
+as a resource.
+"""
+import numpy as np
+
 
 class NeuralNetwork():
+    """
+    """
 
-    def __init__(self):
-        //weights = numpy matrix
-        //biases = numpy vector (matrix?)
-        //initialize hyperparameters to a default setting
+    def __init__(self, dims, learning_rate=0.05):
+        """
+        Initializes an instance of the NeuralNetwork class.
+
+        :param dims: list containing the number of nodes in each layer of the
+            desired network (one int for each layer)
+        :param learning_rate: rate the network learns from training data with
+            default value = 0.05
+        """
+
+        '''
+        self.num_input = num_input
+        self.num_output = num_output
+        self.num_hidden_layers = num_hidden_layers
+        self.hidden_layer_dims = hidden_layer_dims
+        self.learning_rate = learning_rate
+
+        self.weights = np.random.rand((3, 2))
+        self.biases = np.random.rand(())
+        '''
+
+        self.dims = dims
+        self.learning_rate = learning_rate
+        self.weights = []
+        self.initWeightMatrices()
+
+    def initWeightMatrices(self):
+        weights = []
+        for i in range(len(self.dims)-1):
+            cur_weight_matrix = np.random.rand(self.dims[i+1], self.dims[i])
+            weights.append(cur_weight_matrix)
+        self.weights = weights
 
     def feed_forward(self):
         return null
@@ -27,3 +60,13 @@ class NeuralNetwork():
     def backpropagation(self):
         return self
 
+    def print_weight_matrix(self):
+
+        for i in range(len(self.weights)):
+            print("Layer " + str(i + 1) + ": \n")
+            print(self.weights[i])
+            print()
+
+
+myNetwork = NeuralNetwork([])
+myNetwork.print_weight_matrix()
